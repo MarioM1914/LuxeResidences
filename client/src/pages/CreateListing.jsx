ce9bd755e6344a1b3b52c13b6f5b93e24d1761b4
MarioM1914 <maricmario60.mm@gmail.com>
2024-01-21 13:46:00 +0100

    add image slider to the listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -51,7 +51,7 @@ export default function CreateListing() {
           setUploading(false);
         })
         .catch((err) => {
-          setImageUploadError("Image upload failed (2MB max per image)");
+          setImageUploadError("Image upload failed (5MB max per image)");
           setUploading(false);
         });
     } else {
